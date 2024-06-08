7a254903a513cc6e0ac1be0e6620a3c884e3e0c6
delat04 <129554505+delat04@users.noreply.github.com>
2024-06-08 02:14:40 +0100

    Create get-data.js

---
+++ Netlify/get-data.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+exports.handler = async (event, context) => {
+    // Read the JSON file from the server filesystem
+    const data = fs.readFileSync('casas.json', 'utf8');
+    
+    return {
+        statusCode: 200,
+        body: data
+    };
+};
