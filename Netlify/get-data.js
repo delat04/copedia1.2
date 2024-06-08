eff9a1648cea27fbc6a431e9ea1dd7d978f80c20
delat04 <129554505+delat04@users.noreply.github.com>
2024-06-08 03:32:10 +0100

    Update get-data.js

--- Netlify/get-data.js
+++ Netlify/get-data.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
-app.get('/data', (req, res) => {
-    // Read the JSON file from the server filesystem
-    fs.readFile('casas.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        // Send the JSON data as a response to the client
-        res.json(JSON.parse(data));
-    });
-});
+exports.handler = async (event, context) => {
+    try {
+        // Construct the absolute file path to casas.json
+        const filePath = path.resolve(__dirname, 'casas.json');
+
+        // Read the contents of casas.json file
+        const data = fs.readFileSync(filePath, 'utf8');
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (err) {
+        return { statusCode: 500, body: err.toString() };
+    }
+};
