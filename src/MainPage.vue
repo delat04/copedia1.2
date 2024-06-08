80bf92fea72e3cf725c7a7cb34afb8ab62efb95f
delat04 <129554505+delat04@users.noreply.github.com>
2024-06-08 03:00:43 +0100

    Update MainPage.vue

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -259,7 +259,7 @@ function showPopup(latlng, marker, index) {
 onMounted(async () => {
   adjustMapHeight();
   try {
-    axios.get("./Netlify/get-data.js") // Updated URL for Netlify function
+    axios.get("/.netlify/functions/get-data")
         .then(response => {
           initiateMap(response.data); // Assuming response.data is the marker data
         })
