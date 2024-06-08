0ef9c4ed29805c31789e51cbffa877d6b0c2abe5
delat04 <129554505+delat04@users.noreply.github.com>
2024-06-08 03:27:24 +0100

    Update get-data.js

--- Netlify/get-data.js
+++ Netlify/get-data.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 exports.handler = async (event, context) => {
     // Read the JSON file from the server filesystem
-    const data = fs.readFileSync('casas.json', 'utf8');
+    const data = fs.readFileSync('./casas.json', 'utf8');
     
     return {
         statusCode: 200,
